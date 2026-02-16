e5379137ca15d445e5e92793c3b1006782a9fd38
ivan_rn_dev <ivan.mikhailenko@guild-of-developers.ru>
2026-02-16 21:11:34 +0300

    Working at cat profile continuing

--- src/app/(profile)/catProfile.tsx
+++ src/app/(profile)/catProfile.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { View, StyleSheet, Dimensions, Text } from "react-native";
+import { View, StyleSheet, Dimensions, Text, ScrollView } from "react-native";
 import { useLocalSearchParams } from "expo-router";
-import { ActivityIndicator } from "react-native-paper";
+import { ActivityIndicator, ProgressBar } from "react-native-paper";
 import useStore from "../../store/store";
 import ProfileTopBar from "../../components/TopBar/ProfileTopBar";
 import Colors from "../../constants/colors";
@@ -19,45 +19,110 @@ const CatProfile = () => {
 
   const cat = cats.find((cat) => cat.id === catId);
   const breeds = cat?.breeds[0];
+  console.log(breeds);
 
   return (
     <View>
       <ProfileTopBar />
-      <Image
-        style={{ ...styles.image, width: imageWidth, height: imageWidth }}
-        source={cat?.url}
-        placeholder={{ blurhash }}
-        contentFit="cover"
-        cachePolicy={"memory-disk"}
-        onLoadEnd={() => setIsImageLoading(false)}
-        transition={1000}
-        onError={() => {
-          setIsImageLoading(false);
-          setIsImageLoadingError(true);
-        }}
-      />
-      {breeds && (
-        <View style={styles.infoContainer}>
-          <Text style={styles.header}>{breeds.name}</Text>
-          <Text selectable style={styles.text}>
-            {breeds.description}
-          </Text>
-          <Text style={styles.header}>Temperament</Text>
-          <Text selectable style={styles.text}>
-            {breeds.temperament}
-          </Text>
-          <Text style={styles.header}>Origin</Text>
-          <Text selectable style={styles.text}>
-            {breeds.origin}
-          </Text>
-          
-        </View>
-      )}
-      {isImageLoading && (
-        <View style={styles.loaderContainer}>
-          <ActivityIndicator size={"large"} />
-        </View>
-      )}
+      <ScrollView>
+        <Image
+          style={{ ...styles.image, width: imageWidth, height: imageWidth }}
+          source={cat?.url}
+          placeholder={{ blurhash }}
+          contentFit="cover"
+          cachePolicy={"memory-disk"}
+          onLoadEnd={() => setIsImageLoading(false)}
+          transition={1000}
+          onError={() => {
+            setIsImageLoading(false);
+            setIsImageLoadingError(true);
+          }}
+        />
+        {breeds && (
+          <View style={styles.infoContainer}>
+            <Text style={styles.header}>{breeds.name}</Text>
+            <Text selectable style={styles.text}>
+              {breeds.description}
+            </Text>
+            <Text style={styles.header}>Temperament</Text>
+            <Text selectable style={styles.text}>
+              {breeds.temperament}
+            </Text>
+            <Text style={styles.header}>Origin</Text>
+            <Text selectable style={styles.text}>
+              {breeds.origin}
+            </Text>
+            <View>
+              <Text style={styles.header}>Adaptability</Text>
+              <ProgressBar
+                progress={(breeds.adaptability * 20) / 100}
+                color={Colors.accent}
+                style={styles.progressBar}
+              />
+              <Text style={styles.markText}>{`${breeds.adaptability} out of 5`}</Text>
+            </View>
+            <View>
+              <Text style={styles.header}>Affection level</Text>
+              <ProgressBar
+                progress={(breeds.affection_level * 20) / 100}
+                color={Colors.accent}
+                style={styles.progressBar}
+              />
+              <Text style={styles.markText}>{`${breeds.affection_level} out of 5`}</Text>
+            </View>
+            <View>
+              <Text style={styles.header}>Energy level</Text>
+              <ProgressBar
+                progress={(breeds.energy_level * 20) / 100}
+                color={Colors.accent}
+                style={styles.progressBar}
+              />
+              <Text style={styles.markText}>{`${breeds.energy_level} out of 5`}</Text>
+            </View>
+            <View>
+              <Text style={styles.header}>Dog friendly</Text>
+              <ProgressBar
+                progress={(breeds.dog_friendly * 20) / 100}
+                color={Colors.accent}
+                style={styles.progressBar}
+              />
+              <Text style={styles.markText}>{`${breeds.dog_friendly} out of 5`}</Text>
+            </View>
+            <View>
+              <Text style={styles.header}>Child friendly</Text>
+              <ProgressBar
+                progress={(breeds.child_friendly * 20) / 100}
+                color={Colors.accent}
+                style={styles.progressBar}
+              />
+              <Text style={styles.markText}>{`${breeds.child_friendly} out of 5`}</Text>
+            </View>
+            <View>
+              <Text style={styles.header}>Grooming</Text>
+              <ProgressBar
+                progress={(breeds.grooming * 20) / 100}
+                color={Colors.accent}
+                style={styles.progressBar}
+              />
+              <Text style={styles.markText}>{`${breeds.grooming} out of 5`}</Text>
+            </View>
+            <View>
+              <Text style={styles.header}>Health issues</Text>
+              <ProgressBar
+                progress={(breeds.health_issues * 20) / 100}
+                color={Colors.accent}
+                style={styles.progressBar}
+              />
+              <Text style={styles.markText}>{`${breeds.health_issues} out of 5`}</Text>
+            </View>
+          </View>
+        )}
+        {isImageLoading && (
+          <View style={styles.loaderContainer}>
+            <ActivityIndicator size={"large"} />
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 };
@@ -79,6 +144,7 @@ const styles = StyleSheet.create({
   },
   infoContainer: {
     paddingHorizontal: 16,
+    paddingBottom: 200,
   },
   header: {
     fontSize: fontSizes.FONT32,
@@ -90,7 +156,20 @@ const styles = StyleSheet.create({
     fontSize: fontSizes.FONT14,
     fontFamily: "ShantellRegular",
     color: Colors.mainText,
-    textAlign: 'justify',
+    textAlign: "justify",
+  },
+  markText: {
+    color: Colors.accent2,
+    position: "absolute",
+    alignSelf: "center",
+    bottom: 7,
+    fontSize: fontSizes.FONT12,
+    fontFamily: "ShantellBold",
+  },
+  progressBar: {
+    height: 20,
+    borderRadius: 10,
+    marginVertical: 5,
   },
 });
 

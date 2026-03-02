a217b4dbcdcf29749524a87f0838b2ba92355873
ivan_rn_dev <ivan.mikhailenko@guild-of-developers.ru>
2026-03-02 21:27:14 +0300

    Start working at upload images

--- src/app/(main)/upload.tsx
+++ src/app/(main)/upload.tsx
@@ -1,25 +1,20 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Colors from "../../constants/colors";
-import { signOut } from "firebase/auth";
-import { auth } from "../../../firebaseConfig";
+import TopBar from "../../components/TopBar/TopBar";
 import { Button } from "react-native-paper";
+import fontSizes from "../../constants/fontSizes";
 
 const Upload = () => {
-  const logout = async () => {
-    try {
-      await signOut(auth);
-    } catch (error) {
-      console.log("Ошибка");
-    }
-  };
+  const [numColumns, setNumOfColumns] = useState(2);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Настройки</Text>
-      <Button mode={"contained"} onPress={logout}>
-        Выход
-      </Button>
+      <TopBar setNumOfColumns={setNumOfColumns} />
+
+      <TouchableOpacity style={styles.plusButtonContainer}>
+        <Text style={styles.text}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -28,10 +23,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.main,
+  },
+  plusButtonContainer: {
+    position: "absolute",
+    width: 80,
+    height: 80,
+    bottom: 130,
+    right: 16,
+    borderRadius: 40,
+    backgroundColor: Colors.white,
     alignItems: "center",
     justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   text: {
+    fontSize: fontSizes.FONT32,
     color: Colors.mainText,
   },
 });
